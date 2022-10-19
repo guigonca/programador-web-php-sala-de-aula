d073608e3618455129dfc86abcccc71b4d9a57d4
Guilherme Gonçalves <guilhermepgx20@gmail.com>
2022-10-19 20:53:05 -0300

    Atualização

--- rotinas/funcoes.php
+++ rotinas/funcoes.php
@@ -1,40 +1,72 @@
-<?php date_default_timezone_set("America/Sao_Paulo");
-       
-       function escreva_texto(){
-           echo "<p style='text-align: center;'>Minha Primeira função em PHP</p>";
-        }
-        
-        function escreva_texto2($qualquer_coisa){
-            echo "<p style='text-align: center;'>$qualquer_coisa</p>";
-        }
-        
-        function hoje(){
-            $agora = date('d/m/y H:i');
-            echo "<p style='text-align: center;'>$agora</p>";
-            
-        }
-        function pula_linha($pula){
-            $i = 1;
-            while($i <= $pula){
-                echo "<br>";
-                $i++;
-            }
-        }
-        function soma($soma1, $soma2){
-            $total = $soma1+$soma2;
-            echo " O total da soma de $soma1 + $soma2 é :" .$total;
-            pula_linha(1);
-        }
-        function divisao($div1, $div2){
-            $total = $div1/$div2;
-            echo " O total da divisao de $div1 por $div2 é :" .$total;
-            pula_linha(1);
-        }
-        function sub($sub1, $sub2){
-            $total = $sub1-$sub2;
-            echo " $sub1- $sub2 :" .$total;
-            pula_linha(1);
-        }
+<?php
+//Informar o local do projeto
+echo date_default_timezone_get();
+echo "<br>";
+date_default_timezone_set("America/Sao_Paulo");
+echo date_default_timezone_get();
+echo "<br>";
 
-        
-        
+function escreva_texto(){
+echo "<h4 style='text-align: center;'>Minha primeira função em PHP</h4>";
+}
+
+function escreva_texto2($qualquer_coisa){
+echo "<h4 style='text-align: center;'>$qualquer_coisa</h4>";
+}
+
+function hoje(){
+$agora = date('d/m/Y H:i');
+echo "<h4 style='text-align: center;'>$agora</h4>";
+}
+
+function pulaLinha($numero){
+    // if($numero < 1){
+    //     echo "-<br>";
+    // } else {
+    //    for($i=1; $i <= $numero; $i++){
+    //     echo ".<br>";
+    //    }
+    // }
+        do{
+            echo "<br>";
+            $numero--;
+        }while($numero >= 1);
+
+}
+
+function soma($num1, $num2){
+    $total = $num1 + $num2;
+    echo "<h4 style='text-align: center;'>A soma de $num1 + $num2 = $total </h4>";
+    pulaLinha(0);
+}
+
+function soma2(){
+    $valores = func_get_args();
+    $qtd1 = count($valores);
+    $qtd2 = func_num_args();
+    $total = 0;
+    for($i =0; $i < $qtd1; $i++){
+    $total += $valores[$i];
+    echo $valores[$i].( $i == $qtd1 -1 ? " = " :  " + "); 
+    }
+    echo "$total";
+}
+
+function divisao($a, $b){
+    if($b == 0){        
+    echo "ERRO";    
+    } else{
+        $total = $a / $b;
+    echo "Total da divisão entre $a e $b é: ". $total;
+    }
+}
+function multi($a, $b){
+    $total = $a * $b;
+    echo "A multiplicação de $a por $b é: ". $total;
+    
+}
+function subtracao($a, $b){
+    $total = $a-$b;
+    echo "$total";
+
+}
---
+++ rotinas/rotinas2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css" />
+    <title>Senac - Curso de PHP</title>
+</head>
+
+<body>
+    <div>
+        <h1 style="text-align: center;">Procedimentos, funções e métodos.</h1>
+        <h4 style="text-align: center;">restando funções</h4>
+        <hr>
+        <br>
+        <pre>
+            <?php
+
+                include "funcoes2.php";
+                // include "funcoes.php";
+
+                
+                // $texto = escreva_texto4("Texto 4");
+                // echo $texto;
+                
+                $num = subtracao(10,7);
+                echo "A subtração é: ".$num;
+                
+            ?>
+        </pre>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
