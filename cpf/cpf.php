0118dce1df648d1178ac54bfab8a04ca3cc48ddd
Guilherme Gonçalves <guilhermepgx20@gmail.com>
2022-10-28 13:44:34 -0300

    desafio cpf
    Concluindo o desafio do cpf

---
+++ cpf/cpf.php
@@ -0,0 +1,31 @@
+<?php
+    $cpf = "130.591.437-65";
+    $cpf = preg_replace('/[^0-9]/', '', $cpf);
+    $d1 = 0;
+    $d2 = 0;
+    for($i =0, $x=10 ; $i<=8; $i++, $x--){
+        $d1 += $cpf[$i]*$x;
+    }
+
+    for($i =0, $x=11 ; $i<=9; $i++, $x--){
+        $d2 += $cpf[$i]*$x;
+
+        if(str_repeat($i, 11)== $cpf){
+            echo "false";
+            return false;
+        }
+
+    }
+    
+
+    $somaD1= (($d1%11) <2) ? 0 : 11- ($d1%11);
+    $somaD2= (($d2%11) <2) ? 0 : 11- ($d2%11);
+    
+
+    if($somaD1 != $cpf[9] || $somaD2 != $cpf[10]){
+        echo "CPF inválido!";
+
+    } else{
+        echo "CPF válido!";
+    }
+?>
