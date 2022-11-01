176f370a43a1447d0c9d4304cae480aad1e6fee2
Guilherme Gonçalves <guilhermepgx20@gmail.com>
2022-10-31 21:30:54 -0300

    Atualização do site
    aprendendo required

---
+++ projeto/html/header.php
@@ -0,0 +1,17 @@
+<header>
+			<section>
+				<figure id="logo">
+					<img src="img/favicon.ico" alt="Logo">
+				</figure>
+				<input type="text" name="buscar" id="buscar" placeholder="Busque aqui" >
+				<h1 id="textoEnviamos">Enviamos produtos para todo o território nacional.</h1>
+				<figure style="border: none">
+					<img id="carrinho" src="img/carrinhoCompra.png" alt="">
+					<img id="cliente" src="img/clientes.png" alt="">
+				</figure>
+			</section>
+	<?php 
+		include "html/menu.php";
+	?>
+
+</header>
